64cc27b0f5e912c228eed6c4c49ae05c6b2eb4f9
Meir Ron <meir.ron@hpe.com>
2015-11-26 13:52:12 +0200

    set the correct SaaS URL

--- src/main/java/com/hpe/apppulse/openapi/apppulseopenapi/constants.java
+++ src/main/java/com/hpe/apppulse/openapi/apppulseopenapi/constants.java
@@ -5,8 +5,7 @@ package com.hpe.apppulse.openapi.apppulseopenapi;
  */
 public class constants {
 
-    public static final String APPPULSE_URL_PREFIX="http://myd-vm04003.hpswlabs.adapps.hp.com:8080";
-    //public static final String APPPULSE_URL_PREFIX="http://myd-vm05203.hpswlabs.adapps.hp.com:8080";
+    public static final String APPPULSE_URL_PREFIX="https://emast001pgwp.saas.hp.com:8080";
     public static final String OPEN_API_VERSION = "v1";
     public static final String NO_ANSWER = "";
 }
